010ee295606a90f8e29564d8f651ff8127cabe0f
joskeiner simosa urdaneta <joskeiner.simosa9@gmail.com>
2024-04-14 17:30:43 -0300

    new DTO

---
+++ DTO/CommentDTO.cs
@@ -0,0 +1,7 @@
+namespace APIBiblioteca.DTO{
+    public class CommentDTO{
+        public string? Content {get;set;}
+        public bool Recommend {get;set;}
+        public int BookId {get;set;}
+    }
+}
